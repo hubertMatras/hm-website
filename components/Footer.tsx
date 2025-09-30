c34f91e67cce6c80bb1f75f37a2812996f3ff346
hubertMatras <hubert.matras@sky.uk>
2025-09-30 14:55:21 +0100

    Create Icons for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { GitHubIcon } from "../icons/github";
+import { LinkedInIcon } from "../icons/linkedIn";
 
 export default function Footer() {
   return (
-    <footer className="border-t border-slate-200/60 dark:border-slate-800/60 mt-16">
+    <footer className="border-t border-slate-200/60 mt-16">
       <div className="max-w-none w-full px-4 py-10 flex flex-col md:flex-row items-center justify-between gap-4">
         <p className="text-sm muted">
           © {new Date().getFullYear()} Hubert Matras. All rights reserved.
@@ -10,17 +12,17 @@ export default function Footer() {
         <div className="flex items-center gap-4 text-sm">
           <Link
             href="https://github.com/hubertMatras"
-            className="hover:underline"
+            className="inline-flex items-center"
             target="_blank"
           >
-            GitHub
+            <GitHubIcon className="w-6 h-6" />
           </Link>
           <Link
             href="https://www.linkedin.com/in/hubert-matras"
             className="hover:underline"
             target="_blank"
           >
-            LinkedIn
+            <LinkedInIcon className="w-6 h-6" />
           </Link>
         </div>
       </div>
