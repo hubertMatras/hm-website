ef9489b0a93332a27a4268890e391533a6f748dd
hubertMatras <hubert.matras@sky.uk>
2025-09-30 14:35:33 +0100

    Create Tech Stack Data file

---
+++ data/tech-stack.ts
@@ -0,0 +1,34 @@
+export const techStack = {
+    proficient: [
+        "TypeScript",
+        "JavaScript",
+        "React",
+        "Next.js",
+        "OpenAI Models",
+        "OpenAI Assistant and Completions API",
+        "Octopus",
+        "Concourse",
+        "Git"
+    ],
+    workedWith: [
+        "Python",
+        "Svelte",
+        "Java",
+        "NodeJS",
+        "Express",
+        "MongoDB",
+        "Jenkins",
+        "AWS",
+        "SpringBoot",
+        "Haskell",
+        "Prolog",
+        "MySQL",
+        "PHP",
+        "Bash",
+        "Figma"
+    ],
+    languages: [
+        "English",
+        "Polish"
+    ]
+}
