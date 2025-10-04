7fd4b3eda26fc0f92d56f2046384125ee24c4f3e
hubertMatras <hubert.matras@sky.uk>
2025-10-04 16:26:46 +0100

    Create Accordion component

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export { default as AboutMe } from './About'
+export { default as Accordion } from './Accordion'
 export { default as Contact } from './Contact'
 export { default as Experience } from './Experience'
 export { default as Footer } from './Footer'
