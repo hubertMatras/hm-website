9dcb4373e39d525f95692a833de3ceae2eeee255
hubertMatras <hubert.matras@sky.uk>
2025-10-04 15:12:52 +0100

    Change the page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import { AboutMe, Experience, Hero, Projects } from "../components";
 
 export default function HomePage() {
   return (
-    <>
-      <Hero />
-      <AboutMe />
-      <Experience />
-      <Projects />
-    </>
+    <div className="flex h-screen">
+      <div className="w-1/2 flex items-center justify-center bg-gray-50">
+        <Hero />
+      </div>
+      <div className="w-1/2 overflow-y-auto">
+        <AboutMe />
+        <Experience />
+        <Projects />
+      </div>
+    </div>
   );
 }
