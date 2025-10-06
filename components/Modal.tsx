135d4d64e45318aee49e79962c8c6ebaadd17908
hubertMatras <hubert.matras@sky.uk>
2025-10-06 17:00:41 +0100

    Change modal bg color

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -44,7 +44,7 @@ export default function Modal({ isOpen, onClose, project }: ModalProps) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
       <div
-        className="absolute inset-0 bg-blue-100"
+        className="absolute inset-0 bg-black/70"
         onClick={onClose}
       />
       <div className="relative bg-white rounded-lg max-w-2xl w-full mx-4 max-h-[90vh] overflow-y-auto">
