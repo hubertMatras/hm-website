a0de60677a78d59ff6c60a37288fac66b15ef128
hubertMatras <hubert.matras@sky.uk>
2025-10-06 16:41:46 +0100

    update Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,27 +3,25 @@ import { GitHubIcon, LinkedInIcon } from "../icons";
 
 export default function Footer() {
   return (
-    <footer className="lg:border-t border-slate-200/60">
-      <div className="w-full px-4 py-6 flex flex-col lg:flex-row items-center justify-between gap-4">
-        <p className="text-sm muted">
-          © {new Date().getFullYear()} Hubert Matras. All rights reserved.
-        </p>
-        <div className="flex items-center gap-4 text-sm">
-          <Link
-            href="https://github.com/hubertMatras"
-            className="inline-flex items-center"
-            target="_blank"
-          >
-            <GitHubIcon className="w-6 h-6" />
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/hubert-matras"
-            className="hover:underline"
-            target="_blank"
-          >
-            <LinkedInIcon className="w-6 h-6" />
-          </Link>
-        </div>
+    <footer className="px-4 py-6 flex flex-col lg:flex-row items-center justify-center gap-4">
+      <p className="text-sm muted">
+        © {new Date().getFullYear()} Hubert Matras
+      </p>
+      <div className="flex items-center gap-4 text-sm">
+        <Link
+          href="https://github.com/hubertMatras"
+          className="inline-flex items-center"
+          target="_blank"
+        >
+          <GitHubIcon className="w-6 h-6" />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/hubert-matras"
+          className="hover:underline"
+          target="_blank"
+        >
+          <LinkedInIcon className="w-6 h-6" />
+        </Link>
       </div>
     </footer>
   );
